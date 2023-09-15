d4e9235df2557c9b68639b1a5e5c96d2e0c25afa
Devajit <patar1447@gmail.com>
2023-09-15 23:13:40 +0530

    CategoryActivity for showa number of category added

---
+++ app/src/main/java/com/example/maihang/ui/CategoryMealActivity.kt
@@ -0,0 +1,12 @@
+package com.example.maihang.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maihang.R
+
+class CategoryMealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_meal)
+    }
+}
