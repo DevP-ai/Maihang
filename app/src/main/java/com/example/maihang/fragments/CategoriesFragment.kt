52c62aeb1fe64384a1a6dee3d4f1eadddd6234ce
Devajit <patar1447@gmail.com>
2023-09-21 09:22:41 +0530

    Meal listing recyclerView at Category Fragment and Click Event listener of the Category Meal implemented

--- app/src/main/java/com/example/maihang/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/maihang/fragments/CategoriesFragment.kt
@@ -1,18 +1,34 @@
 package com.example.maihang.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.maihang.R
+import com.example.maihang.adapter.CategoryMealAdapter
+import com.example.maihang.databinding.FragmentCategoriesBinding
+import com.example.maihang.ui.CategoryMealActivity
+import com.example.maihang.ui.MainActivity
+import com.example.maihang.ui.MealActivity
+import com.example.maihang.viewModel.HomeViewModel
 
 
 class CategoriesFragment : Fragment() {
+    private lateinit var binding:FragmentCategoriesBinding
+    private lateinit var categoryMealAdapter: CategoryMealAdapter
+    private lateinit var viewModel:HomeViewModel
 
+    companion object{
+        const val CATEGORY_NAME="com.example.maihang.fragments.categoryName"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel=(activity as MainActivity).viewModel
     }
 
     override fun onCreateView(
@@ -20,7 +36,41 @@ class CategoriesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+        binding=FragmentCategoriesBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prepareRecyclerView()
+
+        observeCategory()
+
+        onClickCategory()
+    }
+
+    private fun onClickCategory() {
+        categoryMealAdapter.onItemClick={ category ->
+            val intent=Intent(activity,CategoryMealActivity::class.java)
+            intent.putExtra(CATEGORY_NAME,category.strCategory)
+            startActivity(intent)
+        }
+    }
+
+    private fun observeCategory() {
+        viewModel.observeMealCategoriesLiveData().observe(viewLifecycleOwner, Observer { categories->
+           categoryMealAdapter.setCategories(categories)
+        })
+    }
+
+    private fun prepareRecyclerView() {
+        categoryMealAdapter= CategoryMealAdapter()
+        binding.rvCategory.apply {
+             layoutManager=GridLayoutManager(context,3,GridLayoutManager.VERTICAL,false)
+            adapter=categoryMealAdapter
+        }
     }
 
 }
